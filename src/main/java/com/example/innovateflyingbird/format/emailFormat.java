e6424d73cf6e1ebed24d03044b1af767294f7236
Krystal <1760739491@qq.com>
2022-09-02 11:16:48 +0800

    完成了注册登录系统的编写

--- src/main/java/com/example/innovateflyingbird/format/emailFormat.java
+++ src/main/java/com/example/innovateflyingbird/format/emailFormat.java
@@ -2,7 +2,7 @@ package com.example.innovateflyingbird.format;
 
 public class emailFormat {
     public static boolean isEmail(String str) {
-       
+
 
         String tegex="[a-zA-Z0-9_]+@\\w+(\\.com|\\.cn){1}";
         boolean flag=str.matches(tegex);
