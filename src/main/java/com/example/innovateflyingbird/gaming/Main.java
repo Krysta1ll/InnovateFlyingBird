13669acfbb5e45699171a2aa9a96acd8b0f5f860
Krystal <1760739491@qq.com>
2022-09-05 14:58:26 +0800

    -完成了排行榜系统

--- src/main/java/com/example/innovateflyingbird/gaming/Main.java
+++
@@ -1,8 +0,0 @@
-package com.example.innovateflyingbird.gaming;
-public class Main {
-    public static void main(String[] args) {
-        GameFrame frame = new GameFrame();
-        frame.setVisible(true);
-
-    }
-}
